f0f9d3d9b7609bbfa6e09060144c692360f5ea53
glemysson-Hacker <glemyson20@gmail.com>
2017-02-14 12:19:19 -0300

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo "Removendo CONFVPS 2.0"
+rm /bin/squid3-installer
+rm /bin/user-create
+rm /bin/CONFVPS
+rm /bin/user-del
+rm /bin/monitor
+rm /bin/limite
+rm installer.sh
+rm uninstall
+echo "CONFVPS 2.0 foi removido com sucesso..."
